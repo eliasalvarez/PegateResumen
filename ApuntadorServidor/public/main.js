81b1d7aa6a17ff12176ec65c984ec82c07f65b8e
eliasalvarez <locura.elias@outlook.com>
2014-12-20 02:19:22 -0300

    modificacion
    modificaciones al index.html y al main.js

--- ApuntadorServidor/public/main.js
+++ ApuntadorServidor/public/main.js
@@ -17,3 +17,27 @@ function mainController($scope, $http){
     });
     
     //Cuando se añade un nuevo TODO, manda el texto a la API
+    $scope.createTodo = function(){
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.todos = data;
+                console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        })
+    };
+    
+    //Borra un TODO despues de checkearlo como acabado
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(){
+                $scope.todos = data;
+                console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error:' + data);
+        });
+    };
+}
