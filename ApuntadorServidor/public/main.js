e711227d791a166d9064e5e7894144b587fd5363
eliasalvarez <locura.elias@outlook.com>
2014-12-21 00:06:04 -0300

    nuevas dependencias y modificaciones
    dependencias para mongoose, segun tengo entendido y modificaciones al
    package(agregadas dependencias), al main.js y al server.js, minimas,
    para ver si funciona y no.. todavia no

--- ApuntadorServidor/public/main.js
+++ ApuntadorServidor/public/main.js
@@ -3,41 +3,41 @@ var angularTodo = angular.module('angularTodo', []);
 
 
 //MAIN CONTROLLER controlador de la aplicación
-function mainController($scope, $http){
+function mainController($scope, $http) {
     $scope.formData = {};
-    
-    //Cuando se cargue la página, pide del API todos los TODos
-    $http.ger('/api/todos')
+
+    // Cuando se cargue la página, pide del API todos los TODOs
+    $http.get('/api/todos')
         .success(function(data) {
-        $scope.todos = data;
-        console.log(data)
-    });
-    .error(function(data) {
-        console.log('Error: ' + data);
-    });
-    
-    //Cuando se añade un nuevo TODO, manda el texto a la API
+            $scope.todos = data;
+            console.log(data)
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // Cuando se añade un nuevo TODO, manda el texto a la API
     $scope.createTodo = function(){
         $http.post('/api/todos', $scope.formData)
             .success(function(data) {
                 $scope.formData = {};
                 $scope.todos = data;
                 console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        })
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
     };
-    
-    //Borra un TODO despues de checkearlo como acabado
+
+    // Borra un TODO despues de checkearlo como acabado
     $scope.deleteTodo = function(id) {
         $http.delete('/api/todos/' + id)
-            .success(function(){
+            .success(function(data) {
                 $scope.todos = data;
                 console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error:' + data);
-        });
+            })
+            .error(function(data) {
+                console.log('Error:' + data);
+            });
     };
 }
--- ApuntadorServidor/server.js
+++ ApuntadorServidor/server.js
@@ -1,61 +1,43 @@
-var express  = require ('express');
-var app      = express();
-var mongoose = require('mongoose');
+//server.js
 
-//conexión con la base de datos
+var express     = require('express');
+var app         = express();
+var mongoose    = require('mongoose');
+
+// Conexión con la base de datos
 mongoose.connect('mongodb://localhost:27017/angular-todo');
 
-//configuracioón
+
+// Configuración
 app.configure(function() {
-    //Localización de los ficheros estáticos
+    // Localización de los ficheros estáticos
     app.use(express.static(__dirname + '/public'));
-    //muestra un log de todos los request en la consola
+    // Muestra un log de todos los request en la consola
     app.use(express.logger('dev'));
-    //permite cambiar el html con el método POST
+    // Permite cambiar el HTML con el método POST
     app.use(express.bodyParser());
-    //simula DELETE y PUT
+    // Simula DELETE y PUT
     app.use(express.methodOverride());
 });
 
-var todo = mongoose.model('todo', { text: String });
 
-//Rutas de nuestro API
-//GET de todos los TODOs
+// Rutas de nuestro API
+// GET de todos los TODOs
 app.get('/api/todos', function(req, res) {
-        Todo.find(function(err, todos) {
-            if(err){
-                res.send(err);
-            }
-            res.json(todos);
+    Todo.find(function(err, todos) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(todos);
     });
 });
 
-
-//POST que crea un TODO y devuelve todos tras la creación
+// POST que crea un TODO y devuelve todos tras la creación
 app.post('/api/todos', function(req, res) {
     Todo.create({
         text: req.body.text,
         done: false
-    }, function(err, todo) {
-        if(err) {
-            res.send(err);
-        }
-
-        Todo.find(function(err, todos){
-            if(err){
-                res.send(err);
-            }
-            res.json(todos);
-        });
-    });
-});
-
-
-//DELETE un TODO específico y devuelve todos tras borrarlo
-app.delete('/api/todos/:todo', function(req,res) {
-    Todo.remove({
-        _id: req.params.todo
-    }, function (err, todo) {
+    }, function(err, todo){
         if(err) {
             res.send(err);
         }
@@ -68,13 +50,39 @@ app.delete('/api/todos/:todo', function(req,res) {
         });
     });
 });
-//carga una vista html simple donde irá nuestra Single App Page
-//Angular manejará el Frontend
-app.get('*', function(req,res) {
+
+// DELETE un TODO específico y devuelve todos tras borrarlo.
+app.delete('/api/todos/:todo', function(req, res) {
+    Todo.remove({
+        _id: req.params.todo
+    }, function(err, todo) {
+        if(err){
+            res.send(err);
+        }
+
+        Todo.find(function(err, todos) {
+            if(err){
+                res.send(err);
+            }
+            res.json(todos);
+        });
+
+    })
+});
+
+// Carga una vista HTML simple donde irá nuestra Single App Page
+// Angular Manejará el Frontend
+app.get('*', function(req, res) {
     res.sendfile('./public/index.html');
+});
+
+//Modelo de datos
+var todo = mongoose.model('todo', {
+    text: String,
+    titulo: String
 });
 
 //escucha en el puerto 8080 y corre el server
 app.listen(8080, function(){
-    console.log('App listening on port 8080');
+    console.log('App listening on port 8888');
 });
